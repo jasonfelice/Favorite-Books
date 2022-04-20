d1511246c9a34e1fe31c2f54c1dc2261ebeefc44
Jason Felice <jasonfelice0@gmail.com>
2022-04-20 03:03:58 -0700

    Refactor to use classes

--- assets/app.js
+++ assets/app.js
@@ -1,50 +1,61 @@
 const addButton = document.getElementById('add-book');
 const inputTitle = document.getElementById('title');
 const inputAuthor = document.getElementById('author');
-let booksArray = [];
 
-if (localStorage.books) {
-  booksArray = JSON.parse(localStorage.books);
-}
+class Book {
+  constructor(title, author) {
+    this.title = title;
+    this.author = author;
+  }
 
-function addBook(title, author) {
-  const booksWrapper = document.querySelector('.books-wrapper');
-  const book = document.createElement('div');
-  const bookTitle = document.createElement('h2');
-  const bookAuthor = document.createElement('p');
-  const remove = document.createElement('button');
-  remove.setAttribute('type', 'button');
-  remove.setAttribute('value', `${title}`);
-  remove.textContent = 'Remove';
-  remove.addEventListener('click', (e) => {
-    e.target.parentNode.remove();
-    booksArray = booksArray.filter((v) => !(v.title === e.target.attributes.value.value));
-    localStorage.setItem('books', JSON.stringify(booksArray));
-  });
-  bookTitle.textContent = title;
-  bookAuthor.textContent = author;
-  book.appendChild(bookTitle);
-  book.appendChild(bookAuthor);
-  book.appendChild(remove);
-  book.appendChild(document.createElement('hr'));
-  booksWrapper.appendChild(book);
-}
+  static booksArray = [];
 
-if (booksArray.length > 0) {
-  for (let i = 0; i < booksArray.length; i += 1) {
-    addBook(booksArray[i].title, booksArray[i].author);
+  addBook() {
+    const booksWrapper = document.querySelector('.books-wrapper');
+    const book = document.createElement('div');
+    const bookTitle = document.createElement('h2');
+    const bookAuthor = document.createElement('p');
+    const remove = document.createElement('button');
+    remove.setAttribute('type', 'button');
+    remove.setAttribute('value', this.title);
+    remove.textContent = 'Remove';
+    remove.addEventListener('click', (e) => {
+      e.target.parentNode.remove();
+      const eventValue = e.target.attributes.value.value;
+      Book.booksArray = Book.booksArray.filter((v) => !(v.title === eventValue));
+      localStorage.setItem('books', JSON.stringify(Book.booksArray));
+    });
+    bookTitle.textContent = this.title;
+    bookAuthor.textContent = this.author;
+    book.appendChild(bookTitle);
+    book.appendChild(bookAuthor);
+    book.appendChild(remove);
+    book.appendChild(document.createElement('hr'));
+    booksWrapper.appendChild(book);
   }
 }
 
 function updateBooks() {
-  booksArray.push({
+  Book.booksArray.push({
     title: `${inputTitle.value}`,
     author: `${inputAuthor.value}`,
   });
-  localStorage.setItem('books', JSON.stringify(booksArray));
+  localStorage.setItem('books', JSON.stringify(Book.booksArray));
 }
 
-addButton.addEventListener('click', updateBooks);
+if (localStorage.books) {
+  Book.booksArray = JSON.parse(localStorage.books);
+}
+
+if (Book.booksArray.length > 0) {
+  for (let i = 0; i < Book.booksArray.length; i += 1) {
+    const currentObj = new Book(Book.booksArray[i].title, Book.booksArray[i].author);
+    currentObj.addBook();
+  }
+}
+
 addButton.addEventListener('click', () => {
-  addBook(inputTitle.value, inputAuthor.value);
+  const currentObj = new Book(inputTitle.value, inputAuthor.value);
+  currentObj.addBook();
+  updateBooks();
 });
